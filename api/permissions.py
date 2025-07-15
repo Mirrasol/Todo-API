6c7e7a998122859eb4825a7e01174a42ff70817f
elena_dobruk <elena.dobruk@yandex.ru>
2025-07-15 21:59:10 +0300

    update custom permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,13 +1,22 @@
 from rest_framework import permissions
 
 
+class IsCreator(permissions.BasePermission):
+    """
+    Object-level permissions for the creator of the object.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'PUT', 'PATCH', 'DELETE']:
+            return obj.creator == request.user
+
+
 class IsCreatorOrExecutor(permissions.BasePermission):
     """
     Object-level permission to determine allowed methods depending
     on the creator/executor status.
     """
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'PUT', 'PATCH']:
+        if request.method in ['GET', 'PATCH']:
             return obj.executor == request.user or obj.creator == request.user
-        elif request.method == 'DELETE':
+        elif request.method == ['DELETE', 'PUT']:
             return obj.creator == request.user
